463f1fedb259dbb73d0217ec3345c97cb879789c
Ben Minnikin <b.minnikin@gmail.com>
2019-11-08 00:36:34 +0000

    Add AttachmentPath model class
    Stores a path to an attachment file for comments and bug reports.

---
+++ BugTracker/Models/AttachmentPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+	public class AttachmentPath
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Path { get; set; }
+	}
+}
