611b472d39a1ca3bafa2379b41189318d1eb1b91
Ben Minnikin <b.minnikin@gmail.com>
2019-11-08 00:40:20 +0000

    Add BugTrackerDbContext model class
    Derives from DbContext in EF.

---
+++ BugTracker/Models/BugTrackerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+	public class BugTrackerDbContext : DbContext
+	{
+		public BugTrackerDbContext(DbContextOptions<BugTrackerDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<BugReport> BugReports { get; set; }
+	}
+}
