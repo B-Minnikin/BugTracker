39046e673d124367f0f9d27b7ff40db34e7b79ff
Ben Minnikin <b.minnikin@gmail.com>
2019-11-08 00:22:07 +0000

    Add and implement BugState model class
    Capture and log changes of state in a bug report and build a history.

---
+++ BugTracker/Models/BugState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+	public class BugState
+	{
+		[Key]
+		public int Id { get; set; }
+		public DateTime Time { get; set; }
+		public string Author { get; set; } // link to profile
+		public string StateName { get; set; }
+	}
+}
