827a71496d3089134cd2488e02fccb7533f95add
Ben Minnikin <b.minnikin@gmail.com>
2019-11-08 00:53:28 +0000

    Fix database service calls in Startup.ConfigureServices
    Updated class names to match current model.

--- BugTracker/Startup.cs
+++ BugTracker/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTracker.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,10 +25,10 @@ namespace BugTracker
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContextPool<BugReportContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnectionString")));
+			services.AddDbContextPool<BugTrackerDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnectionString")));
 
 			services.AddControllersWithViews();
-			services.AddScoped<IBugReportRepository, BugReportRepository>();
+			services.AddScoped<IBugTrackerRepository, BugTrackerRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
