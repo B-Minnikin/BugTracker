d5eb3d6cdfd8304e2aeed14394b347caf3b00608
Ben Minnikin <b.minnikin@gmail.com>
2019-11-24 14:16:59 +0000

    Add BugReportController class

---
+++ BugTracker/Controllers/BugReportController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class BugReportController : Controller
+	{
+		private readonly ILogger<BugReportController> logger;
+
+		public BugReportController(ILogger<BugReportController> logger)
+		{
+			this.logger = logger;
+		}
+
+		[HttpGet]
+		public ViewResult CreateReport()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult CreateReport()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ViewResult EditReport()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult EditReport()
+		{
+			return View();
+		}
+	}
+}
