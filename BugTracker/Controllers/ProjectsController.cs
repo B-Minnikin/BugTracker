139680be9f34e050563b13ae482ad078255c7769
Ben Minnikin <b.minnikin@gmail.com>
2019-11-24 11:20:34 +0000

    Add basis of ProjectsController.
    Starting point for dealing with a user's projects.

---
+++ BugTracker/Controllers/ProjectsController.cs
@@ -0,0 +1,37 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class ProjectsController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public ProjectsController(ILogger<HomeController> logger)									
+		{
+			this._logger = logger;
+		}
+
+		public ViewResult Projects()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ViewResult CreateProject()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult CreateProject(Project project)
+		{
+			return View();
+		}
+	}
+}
