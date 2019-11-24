51afe9669825dd67b67ed5e402d1ac1590df8cdf
Ben Minnikin <b.minnikin@gmail.com>
2019-11-24 12:03:52 +0000

    Add AccountController

---
+++ BugTracker/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly ILogger<AccountController> logger;
+
+		public AccountController(ILogger<AccountController> logger)
+		{
+			this.logger = logger;
+		}
+
+		public ViewResult LogIn()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ViewResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult Register(RegisterViewModel model)
+		{
+			return View();
+		}
+
+		public ViewResult LogOut()
+		{
+			return View();
+		}
+	}
+}
