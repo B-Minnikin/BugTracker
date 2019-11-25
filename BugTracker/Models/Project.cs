b37dcc996a01fe279f653cc54ac9d20ed4a72aa5
Ben Minnikin <b.minnikin@gmail.com>
2019-11-25 08:47:49 +0000

    Add extra properties to Project model class
    CreationTime and LastUpdateTime DateTime properties. String Description for project details.

--- BugTracker/Models/Project.cs
+++ BugTracker/Models/Project.cs
@@ -11,6 +11,9 @@ namespace BugTracker.Models
 		[Key]
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime CreationTime { get; set; }
+		public DateTime LastUpdateTime { get; set; }
 		public bool Hidden { get; set; }
 	}
 }
