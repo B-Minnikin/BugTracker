dc28c0b8e1569048dbde173cfdb6c9616a4a61b5
Ben Minnikin <b.minnikin@gmail.com>
2019-11-25 12:43:19 +0000

    Inject IProjectRepository into ProjectsController

--- BugTracker/Controllers/ProjectsController.cs
+++ BugTracker/Controllers/ProjectsController.cs
@@ -11,10 +11,13 @@ namespace BugTracker.Controllers
 	public class ProjectsController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IProjectRepository projectRepository;
 
-		public ProjectsController(ILogger<HomeController> logger)									
+		public ProjectsController(ILogger<HomeController> logger,
+									IProjectRepository projectRepository)
 		{
 			this._logger = logger;
+			this.projectRepository = projectRepository;
 		}
 
 		public ViewResult Projects()
