90c33825f2049bb80dc6a20e74476a772bf6f2e0
Ben Minnikin <b.minnikin@gmail.com>
2019-12-18 22:22:24 +0000

    Install System.Data.SqlClient nuget package and implement Add in dapper project repository

--- BugTracker/Models/DapperProjectRepository.cs
+++ BugTracker/Models/DapperProjectRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +11,14 @@ namespace BugTracker.Models
 	{
 		public Project Add(Project project)
 		{
-			throw new NotImplementedException();
+			using (IDbConnection connection = new System.Data.SqlClient.SqlConnection("server=(localdb)\\MSSQLLocalDB;database=BugTrackerDB_cookies;Trusted_Connection=true")) // rather than static helper class function - string here
+			{
+				//var query = connection.Query<Project>("Select * FROM Projects");
+				var result = connection.Execute("dbo.Projects_Insert @Name @Description @CreationTime @LastUpdateTime @Hidden",
+					new { Name = project.Name, Description = project.Description, CreationTime = project.CreationTime, LastUpdateTime = project.LastUpdateTime, Hidden = project.Hidden });
+				var query = connection.QueryFirst<Project>("dbo.Projects_GetProject @ProjectId", new { ProjectId = result });
+				return query;
+			}
 		}
 
 		public Project Delete(int Id)
