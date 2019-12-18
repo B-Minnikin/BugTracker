8e728a9349f400aa0e44e9fa2a5b319f7f580152
Ben Minnikin <b.minnikin@gmail.com>
2019-12-18 22:23:23 +0000

    Inject DapperProjectRepository from ConfigureServices

--- BugTracker/Startup.cs
+++ BugTracker/Startup.cs
@@ -26,7 +26,7 @@ namespace BugTracker
 		{
 
 			services.AddControllersWithViews();
-			services.AddScoped<IProjectRepository, ProjectRepository>();
+			services.AddScoped<IProjectRepository, DapperProjectRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
