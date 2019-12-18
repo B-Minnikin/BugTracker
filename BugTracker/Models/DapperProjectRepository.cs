34953f08460ac3ed4402f25226c00e472b0df0b2
Ben Minnikin <b.minnikin@gmail.com>
2019-12-18 21:23:00 +0000

    DapperProjectRepository implements IProjectRepository
    All methods throw NotImplementedException.

--- BugTracker/Models/DapperProjectRepository.cs
+++ BugTracker/Models/DapperProjectRepository.cs
@@ -5,8 +5,31 @@ using System.Threading.Tasks;
 
 namespace BugTracker.Models
 {
-	public class DapperProjectRepository
+	public class DapperProjectRepository : IProjectRepository
 	{
+		public Project Add(Project project)
+		{
+			throw new NotImplementedException();
+		}
 
+		public Project Delete(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Project> GetAllProjects()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Project GetProject(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Project Update(Project projectChanges)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
