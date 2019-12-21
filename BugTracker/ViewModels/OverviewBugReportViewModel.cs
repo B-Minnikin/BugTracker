e092f739ccabf2ced420150f8d04aceea21ce415
Ben Minnikin <b.minnikin@gmail.com>
2019-12-21 04:30:14 +0000

    Add property definitions to OverviewBugReportViewModel

--- BugTracker/ViewModels/OverviewBugReportViewModel.cs
+++ BugTracker/ViewModels/OverviewBugReportViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace BugTracker.ViewModels
 {
 	public class OverviewBugReportViewModel
 	{
+		BugReport bugReport { get; set; }
+		List<BugReportComment> BugReportComments { get; set; }
+		List<BugState> BugStates { get; set; }
+		List<AttachmentPath> AttachmentPaths { get; set; }
 	}
 }
