bbf88b34f8e67f5787d8ceb936712ed362d57595
Ben Minnikin <b.minnikin@gmail.com>
2019-12-21 17:54:00 +0000

    Add CommentController class

---
+++ BugTracker/Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class CommentController : Controller
+	{
+		private readonly ILogger<CommentController> _logger;
+		private readonly IProjectRepository projectRepository;
+
+		public CommentController(ILogger<CommentController> logger,
+									IProjectRepository projectRepository)
+		{
+			this._logger = logger;
+			this.projectRepository = projectRepository;
+		}
+
+	}
+}
