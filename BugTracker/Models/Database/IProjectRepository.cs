6ae7b5e39a6188fcf913bd89b8ea62ac46191f18
Ben Minnikin <b.minnikin@gmail.com>
2019-12-28 23:44:40 +0000

    Add GetCommentCountById to IProjectRepository
    Counts all comments associated with a bug report, returns int.

--- BugTracker/Models/Database/IProjectRepository.cs
+++ BugTracker/Models/Database/IProjectRepository.cs
@@ -20,6 +20,7 @@ namespace BugTracker.Models
 		BugReport GetBugReportById(int bugReportId);
 		BugReport UpdateBugReport(BugReport reportChanges);
 		BugReport DeleteBugReport(int id);
+		int GetCommentCountById(int BugReportId);
 
 		// Bug Report Comments
 		BugReportComment CreateComment(BugReportComment bugReportComment);
