50f03f50ee1167ea50d6b0fafd3a1ace4934184a
Ben Minnikin <b.minnikin@gmail.com>
2019-12-31 05:02:10 +0000

    Add EditBugReportViewModel class
    Fed to the Edit view. Contains a BugReport for modification, along with the current state of the bug report as held in an enum. This property is separate from the BugReport due to database design.

---
+++ BugTracker/ViewModels/EditBugReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.ViewModels
+{
+	public class EditBugReportViewModel
+	{
+		BugReport BugReport { get; set; }
+		StateType CurrentState { get; set; }
+	}
+}
