78af10c2d58866221750b23de85c683308d3d2e9
Ben Minnikin <b.minnikin@gmail.com>
2020-01-05 17:04:12 +0000

    Remove unneeded BugReport collections

--- BugTracker/Models/BugReport.cs
+++ BugTracker/Models/BugReport.cs
@@ -20,9 +20,5 @@ namespace BugTracker.Models
 		public string PersonReporting { get; set; }
 		
 		public int ProjectId { get; set; }
-
-		public IEnumerable<BugReportComment> Comments { get; set; }
-		public IEnumerable<BugState> StateHistory { get; set; }
-		public IEnumerable<AttachmentPath> AttachmentPaths { get; set; }
 	}
 }
