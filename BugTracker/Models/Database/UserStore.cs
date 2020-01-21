ca2ef90fa1d0e18ccd83cc8e884d8caa785e0296
Ben Minnikin <b.minnikin@gmail.com>
2020-01-21 20:54:04 +0000

    UserStore implements IUserStore
    Generate unimplemented methods for IUserStore.

--- BugTracker/Models/Database/UserStore.cs
+++ BugTracker/Models/Database/UserStore.cs
@@ -1,11 +1,99 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BugTracker.Models.Database
 {
-	public class UserStore
+	public class UserStore : IUserStore<IdentityUser>
 	{
+		public Task<IdentityResult> CreateAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityResult> DeleteAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetNormalizedUserNameAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetUserIdAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetUserNameAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SetNormalizedUserNameAsync(IdentityUser user, string normalizedName, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SetUserNameAsync(IdentityUser user, string userName, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IdentityResult> UpdateAsync(IdentityUser user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~UserStore()
+		// {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+
 	}
 }
