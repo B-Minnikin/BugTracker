0e2ee111f02ad779f18d152191e56fc57fe680f0
Ben Minnikin <b.minnikin@gmail.com>
2020-01-22 18:18:44 +0000

    Implement LoginViewModel
    String properties to hold email address and password data.

--- BugTracker/ViewModels/LoginViewModel.cs
+++ BugTracker/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,12 @@ namespace BugTracker.ViewModels
 {
 	public class LoginViewModel
 	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
 
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
 	}
 }
