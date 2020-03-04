6e907ab28700f2fe47e9e2eac1029214af12b4a0
Ben Minnikin <b.minnikin@gmail.com>
2020-03-04 16:04:00 +0000

    Add ApplicationUserManager ctor

--- BugTracker/Models/Authorization/ApplicationUserManager.cs
+++ BugTracker/Models/Authorization/ApplicationUserManager.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BugTracker.Models.Authorization
 {
-	public class ApplicationUserManager
+	public class ApplicationUserManager : UserManager<IdentityUser>
 	{
+		public ApplicationUserManager() : this(new UserStore(), null, null, null, null, null, null, null, null)
+		{
+		}
+
+		public ApplicationUserManager(IUserStore<IdentityUser> store, IOptions<IdentityOptions> optionsAccessor,
+			IPasswordHasher<IdentityUser> passwordHasher, IEnumerable<IUserValidator<IdentityUser>> userValidators,
+			IEnumerable<IPasswordValidator<IdentityUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors,
+			IServiceProvider services, ILogger<UserManager<IdentityUser>> logger) : base(store, optionsAccessor,
+				passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+		{
+		}
 	}
 }
