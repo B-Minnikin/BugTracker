97863cc724e47e7c9950ebba5d4cabe6f06833a6
Ben Minnikin <b.minnikin@gmail.com>
2020-03-04 16:29:19 +0000

    Using directive in ApplicationUserManager
    Can access UserStore class in Models.Authorization folder.

--- BugTracker/Models/Authorization/ApplicationUserManager.cs
+++ BugTracker/Models/Authorization/ApplicationUserManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTracker.Models.Database;
 
 namespace BugTracker.Models.Authorization
 {
