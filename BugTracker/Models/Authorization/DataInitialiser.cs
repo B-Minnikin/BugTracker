8bb1ede036070a6fe3b9f16a74163191bfb84ac8
Ben Minnikin <b.minnikin@gmail.com>
2020-03-04 19:10:17 +0000

    Add SeedRoles method to DataInitialiser
    Checks RoleManager for the existance of role names stored in the Roles enum, and any not found are registered with the RoleManager.

--- BugTracker/Models/Authorization/DataInitialiser.cs
+++ BugTracker/Models/Authorization/DataInitialiser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +14,22 @@ namespace BugTracker.Models.Authorization
 		Member
 	}
 
-	public class DataInitialiser
+	public static class DataInitialiser
 	{
+		public static async Task SeedRoles(IServiceProvider serviceProvider)
+		{
+			using (var scope = serviceProvider.CreateScope())
+			{
+				var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+				foreach (var role in Enum.GetNames(typeof(Roles)))
+				{
+					if(!await roleManager.RoleExistsAsync(role))
+					{
+						await roleManager.CreateAsync(new IdentityRole(role));
+					}
+				}
+			}
+		}
 	}
 }
