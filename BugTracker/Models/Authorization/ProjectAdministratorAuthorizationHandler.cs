56384e3b4a08a61310eb020376fa736243e3ad32
Ben Minnikin <b.minnikin@gmail.com>
2020-03-05 16:40:39 +0000

    Refactor ProjectAdministratorAuthorizationHandler to use AuthorizationHelper

--- BugTracker/Models/Authorization/ProjectAdministratorAuthorizationHandler.cs
+++ BugTracker/Models/Authorization/ProjectAdministratorAuthorizationHandler.cs
@@ -16,8 +16,8 @@ namespace BugTracker.Models.Authorization
 				return Task.CompletedTask;
 			}
 
-			bool userIsSuperadministrator = GetUserInRole(userName, Enum.GetName(typeof(Roles), Roles.Superadministrator), 0).Result;
-			bool userIsProjectAdministrator = GetUserInRole(userName, Enum.GetName(typeof(Roles), Roles.Administrator), projectId).Result;
+			bool userIsSuperadministrator = AuthorizationHelper.UserIsSuperadministrator(userName);
+			bool userIsProjectAdministrator = AuthorizationHelper.UserIsInProjectRole(userName, Enum.GetName(typeof(Roles), Roles.Administrator), projectId);
 
 			if (userIsSuperadministrator || userIsProjectAdministrator)
 			{
@@ -25,25 +25,6 @@ namespace BugTracker.Models.Authorization
 			}
 
 			return Task.CompletedTask;
-		}
-
-		// TODO -- move out to static helper class
-		protected async Task<bool> GetUserInRole(string userName, string roleName, int id)
-		{
-			ApplicationUserManager userManager = new ApplicationUserManager();
-			var user = await userManager.FindByNameAsync(userName);
-			bool userIsInRole;
-
-			if (roleName == "Superadministrator")
-			{
-				userIsInRole = await userManager.IsInRoleAsync(user, roleName);
-			}
-			else
-			{
-				userIsInRole = await userManager.IsInRoleAsync(Int32.Parse(user.Id), roleName, id);
-			}
-
-			return userIsInRole;
 		}
 	}
 }
