49428313c4b757666a410c13cf2fe27a55143e04
Ben Minnikin <b.minnikin@gmail.com>
2020-03-05 08:29:24 +0000

    Implement HandleRequirementAsync in ProjectAccessAuthorizationHandler
    Check whether the user belongs to either the administrator or member roles for the specific project for successful authorization. Modified GetUserInRole to deal with superadministrators not needing to explicitly reference a project ID for successful access rights.

--- BugTracker/Models/Authorization/ProjectAccessAuthorizationHandler.cs
+++ BugTracker/Models/Authorization/ProjectAccessAuthorizationHandler.cs
@@ -8,17 +8,35 @@ namespace BugTracker.Models.Authorization
 {
 	public class ProjectAccessAuthorizationHandler : AuthorizationHandler<ProjectAccessRequirement, int>
 	{
-		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProjectAccessRequirement requirement, int resource)
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProjectAccessRequirement requirement, int projectId)
 		{
-			throw new NotImplementedException();
+			string userName = context.User.Identity.Name;
+			bool userIsSuperadministrator = GetUserInRole(userName, Enum.GetName(typeof(Roles), Roles.Superadministrator), 0).Result;
+			bool userIsProjectAdministrator = GetUserInRole(userName, Enum.GetName(typeof(Roles), Roles.Administrator), projectId).Result;
+			bool userIsProjectMember = GetUserInRole(userName, Enum.GetName(typeof(Roles), Roles.Member), projectId).Result;
+			
+			if(userIsSuperadministrator || userIsProjectAdministrator || userIsProjectMember)
+			{
+				context.Succeed(requirement);
+			}
+
+			return Task.CompletedTask;
 		}
 
 		protected async Task<bool> GetUserInRole(string userName, string roleName, int id)
 		{
 			ApplicationUserManager userManager = new ApplicationUserManager();
 			var user = await userManager.FindByNameAsync(userName);
+			bool userIsInRole;
 
-			bool userIsInRole = await userManager.IsInRoleAsync(Int32.Parse(user.Id), roleName, id);
+			if (roleName == "Superadministrator")
+			{
+				userIsInRole = await userManager.IsInRoleAsync(user, roleName);
+			}
+			else
+			{
+				userIsInRole = await userManager.IsInRoleAsync(Int32.Parse(user.Id), roleName, id);
+			}
 
 			return userIsInRole;
 		}
