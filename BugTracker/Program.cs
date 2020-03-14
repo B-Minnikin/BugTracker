3f8641541a5eb1921e1256444d4a6cf42821e23a
Ben Minnikin <b.minnikin@gmail.com>
2020-03-14 18:45:53 +0000

    Add NLog configuration in the host builder

--- BugTracker/Program.cs
+++ BugTracker/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace BugTracker
 {
@@ -21,6 +22,14 @@ namespace BugTracker
 			  .ConfigureWebHostDefaults(webBuilder =>
 			  {
 				  webBuilder.UseStartup<Startup>();
-			  });
+			  })
+			.ConfigureLogging((hostingContext, logging) =>
+			{
+				logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+				logging.AddConsole();
+				logging.AddDebug();
+				logging.AddEventSourceLogger();
+				logging.AddNLog();
+			});
 	}
 }
