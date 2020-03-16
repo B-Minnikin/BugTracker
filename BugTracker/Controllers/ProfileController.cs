3d5bdb98c87e1f5be9dbf733bbaf6d94a813b3bf
Ben Minnikin <b.minnikin@gmail.com>
2020-03-16 01:54:26 +0000

    Add Edit methods in ProfileController
    Use to edit a user's profile.

--- BugTracker/Controllers/ProfileController.cs
+++ BugTracker/Controllers/ProfileController.cs
@@ -1,4 +1,7 @@
 ﻿using BugTracker.Models;
+using BugTracker.Models.Authorization;
+using BugTracker.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,11 +14,46 @@ namespace BugTracker.Controllers
 	public class ProfileController : Controller
 	{
 		private readonly ILogger<ProfileController> logger;
+		private readonly IAuthorizationService authorizationService;
+		private readonly ApplicationUserManager userManager;
 
 		public ProfileController(ILogger<ProfileController> logger,
-						IProjectRepository projectRepository)
+									IProjectRepository projectRepository,
+									IAuthorizationService authorizationService)
 		{
 			this.logger = logger;
+			this.authorizationService = authorizationService;
+			this.userManager = new ApplicationUserManager();
+		}
+
+		[HttpGet]
+		[Authorize]
+		public IActionResult Edit(int id)
+		{
+			var authorizationResult = authorizationService.AuthorizeAsync(HttpContext.User, id, "CanModifyProfilePolicy");
+			if (authorizationResult.IsCompletedSuccessfully && authorizationResult.Result.Succeeded)
+			{
+
+				return View();
+			}
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpPost]
+		[Authorize]
+		public IActionResult Edit(EditProfileViewModel model)
+		{
+			var authorizationResult = authorizationService.AuthorizeAsync(HttpContext.User, model.id, "CanModifyProfilePolicy");
+			if (authorizationResult.IsCompletedSuccessfully && authorizationResult.Result.Succeeded)
+			{
+				if (ModelState.IsValid)
+				{
+
+				}
+			}
+
+			return View();
 		}
 	}
 }
