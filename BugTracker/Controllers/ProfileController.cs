dbc4e9a8687f319bcf9948ca728ed48c11990867
Ben Minnikin <b.minnikin@gmail.com>
2020-03-16 01:03:17 +0000

    Add ProfileController class
    Use to handle user profiles.

---
+++ BugTracker/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class ProfileController : Controller
+	{
+		private readonly ILogger<ProfileController> logger;
+
+		public ProfileController(ILogger<ProfileController> logger,
+						IProjectRepository projectRepository)
+		{
+			this.logger = logger;
+		}
+	}
+}
