bd5728bf0efdce51d806e388edd1e460230b9505
Ben Minnikin <b.minnikin@gmail.com>
2020-05-14 11:23:40 +0100

    Add EmailHelper class implementing IEmailHelper

---
+++ BugTracker/Models/EmailHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+	public class EmailHelper : IEmailHelper
+	{
+		public void Send(string userName, string emailAddress, string subject, string messageBody)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
