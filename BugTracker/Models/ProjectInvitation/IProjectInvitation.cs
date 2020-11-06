dc02c22ccbc922eb90130c644f795c83a174e960
Ben Minnikin <b.minnikin@gmail.com>
2020-11-06 23:53:24 +0000

    Add ProjectInvitation interface
    To be used for adding and removing email addresses to a repository list of potential users receiving membership permissions for a specific project.

---
+++ BugTracker/Models/ProjectInvitation/IProjectInvitation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models.ProjectInvitation
+{
+	interface IProjectInvitation
+	{
+		void AddProjectInvitation(string emailAddress, int projectId);
+		void RemovePendingProjectInvitation(string emailAddress, int projectId);
+	}
+}
