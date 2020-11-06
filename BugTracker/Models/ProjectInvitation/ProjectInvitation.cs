37c34c7176549cc98921a6aaca872b2243daaba5
Ben Minnikin <b.minnikin@gmail.com>
2020-11-06 23:54:40 +0000

    Add ProjectInvitation class implementing IProjectInvitation

---
+++ BugTracker/Models/ProjectInvitation/ProjectInvitation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models.ProjectInvitation
+{
+	public class ProjectInvitation : IProjectInvitation
+	{
+		public void AddProjectInvitation(string emailAddress, int projectId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemovePendingProjectInvitation(string emailAddress, int projectId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
