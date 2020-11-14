16383ff18014e1d4d2fb8385016ef5a9312432ac
Ben Minnikin <b.minnikin@gmail.com>
2020-11-14 09:20:46 +0000

    Add ProjectInvitations_GetInvitationsForEmailAddress to SQL stored procedures

---
+++ BugTracker/Database/Stored Procedures/ProjectInvitations_GetInvitationsForEmailAddress.sql
@@ -0,0 +1,24 @@
+﻿USE [BugTrackerDB]
+GO
+/****** Object:  StoredProcedure [dbo].[ProjectInvitations_GetInvitationsForEmailAddress]    Script Date: 14/11/2020 09:19:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[ProjectInvitations_GetInvitationsForEmailAddress]
+	@EmailAddress nvarchar(MAX)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT ProjectInvitations.ProjectId
+	FROM ProjectInvitations WHERE ProjectInvitations.EmailAddress = @EmailAddress
+END
