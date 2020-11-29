affdcf3f947e693afc5c3f8743236c9112e93859
Ben Minnikin <b.minnikin@gmail.com>
2020-11-29 22:16:04 +0000

    Add ErrorController to project
    Intended to handle errors, including 404 status codes.

---
+++ BugTracker/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Controllers
+{
+	public class ErrorController : Controller
+	{
+		private readonly ILogger<ErrorController> logger;
+
+		public ErrorController(ILogger<ErrorController> logger)
+		{
+			this.logger = logger;
+		}
+	}
+}
