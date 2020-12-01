7fe5ebf44c788a7cb452397a59d209b419ec4da5
Ben Minnikin <b.minnikin@gmail.com>
2020-12-01 11:03:21 +0000

    Remove Error action method from HomeController
    No longer used - moved functionality to ErrorController.

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -29,11 +29,5 @@ namespace BugTracker.Controllers
 		{
 			return View();
 		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
 	}
 }
