a4bbf03f7f68a6df4f903d0cdba2dd336a8ac851
Ben Minnikin <b.minnikin@gmail.com>
2020-12-07 12:07:05 +0000

    Add stored procedure to incrememnt next free bug report ID

---
+++ BugTracker/Database/Stored Procedures/LocalProjectBugReportIds_IncrementNextFreeId.sql
@@ -0,0 +1,25 @@
+﻿USE [BugTrackerDB]
+GO
+/****** Object:  StoredProcedure [dbo].[LocalProjectBugReportIds_IncrementNextFreeId]    Script Date: 07/12/2020 11:33:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[LocalProjectBugReportIds_IncrementNextFreeId]
+	@ProjectId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE LocalProjectBugReportIds
+	SET NextFreeId = NextFreeId + 1
+	WHERE LocalProjectBugReportIds.ProjectId = @ProjectId
+END
