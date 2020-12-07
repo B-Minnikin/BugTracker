52da9aa8317b582443de12af4ed73f4754e0e2bf
Ben Minnikin <b.minnikin@gmail.com>
2020-12-07 02:41:26 +0000

    Add LocalProjectBugReportIds.sql to Tables folder
    Store next free bug report ID for a specific project so that bug reports have their set of unique IDs. ProjectId is a foreign key referencing a Project's ProjectId.

---
+++ BugTracker/Database/Tables/LocalProjectBugReportIds.sql
@@ -0,0 +1,25 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  Table [dbo].[LocalProjectBugReportIds]    Script Date: 07/12/2020 00:08:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[LocalProjectBugReportIds](
+	[ProjectId] [int] NOT NULL,
+	[NextFreeId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[LocalProjectBugReportIds] ADD  CONSTRAINT [DF_ProjectsNextFreeBugReportId_NextFreeId]  DEFAULT ((1)) FOR [NextFreeId]
+GO
+
+ALTER TABLE [dbo].[LocalProjectBugReportIds]  WITH CHECK ADD  CONSTRAINT [FK_ProjectsNextFreeBugReportId_Projects] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Projects] ([ProjectId])
+GO
+
+ALTER TABLE [dbo].[LocalProjectBugReportIds] CHECK CONSTRAINT [FK_ProjectsNextFreeBugReportId_Projects]
+GO
