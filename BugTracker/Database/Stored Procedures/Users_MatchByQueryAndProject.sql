9c3b607a0411d1bd86f01675d33a6dd43392bf65
Ben Minnikin <b.minnikin@gmail.com>
2020-12-15 22:14:49 +0000

    Add stored procedure to get a collection of user names and email addresses
    Procedure takes a string query and project ID of type integer, returning a collection of user names and email addresses for all partially matched users.

---
+++ BugTracker/Database/Stored Procedures/Users_MatchByQueryAndProject.sql
@@ -0,0 +1,28 @@
+﻿USE [BugTrackerDB]
+GO
+/****** Object:  StoredProcedure [dbo].[Users_MatchByQueryAndProject]    Script Date: 15/12/2020 12:53:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[Users_MatchByQueryAndProject]
+	@Query nvarchar(max),
+	@ProjectId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT Users.UserName, Users.Email
+	FROM Users
+	INNER JOIN UserRoles ON UserRoles.UserId = Users.Id
+	WHERE Users.UserName LIKE ('%' + @Query + '%') AND
+		ProjectId = @ProjectId
+END
