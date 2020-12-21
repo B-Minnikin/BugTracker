9701f70e56cc7cd0ed44fc502573e3634c41e678
Ben Minnikin <b.minnikin@gmail.com>
2020-12-21 08:16:47 +0000

    Add UsersAssignedToBugReport_Insert.sql stored procedure
    Takes integer parameters UserId and BugReportId and inserts those foreign key references to UsersAssignedToBugReport table if an entry does not already exist.

---
+++ BugTracker/Database/Stored Procedures/UsersAssignedToBugReport_Insert.sql
@@ -0,0 +1,33 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[UsersAssignedToBugReport_Insert]    Script Date: 21/12/2020 08:14:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[UsersAssignedToBugReport_Insert]
+	@UserId int,
+	@BugReportId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF NOT EXISTS (SELECT * FROM dbo.UsersAssignedToBugReport
+		WHERE UserId = @UserId AND BugReportId = @BugReportId)
+	BEGIN
+		-- Insert statements for procedure here
+		INSERT INTO dbo.UsersAssignedToBugReport (UserId, BugReportId)
+		VALUES (@UserId, @BugReportId)
+	END
+END
+GO
