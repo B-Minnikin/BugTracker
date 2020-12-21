9cfef8e1f53c6a638e192c29733a3b2d0f74495e
Ben Minnikin <b.minnikin@gmail.com>
2020-12-21 07:09:11 +0000

    Add UsersAssignedToBugReport SQL table
    Holds two foreign keys, mapping a user ID to a bug report ID that the user is assigned to.

---
+++ BugTracker/Database/Tables/UsersAssignedToBugReport.sql
@@ -0,0 +1,32 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  Table [dbo].[UsersAssignedToBugReport]    Script Date: 21/12/2020 07:06:46 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UsersAssignedToBugReport](
+	[UserId] [int] NOT NULL,
+	[BugReportId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[UsersAssignedToBugReport]  WITH CHECK ADD  CONSTRAINT [FK_UsersAssignedToBugReport_BugReport] FOREIGN KEY([BugReportId])
+REFERENCES [dbo].[BugReport] ([BugReportId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[UsersAssignedToBugReport] CHECK CONSTRAINT [FK_UsersAssignedToBugReport_BugReport]
+GO
+
+ALTER TABLE [dbo].[UsersAssignedToBugReport]  WITH CHECK ADD  CONSTRAINT [FK_UsersAssignedToBugReport_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[UsersAssignedToBugReport] CHECK CONSTRAINT [FK_UsersAssignedToBugReport_Users]
+GO
+
+
