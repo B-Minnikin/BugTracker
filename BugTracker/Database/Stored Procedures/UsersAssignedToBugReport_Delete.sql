2f1f3f054f10ebca3213433eaf5d0698a5193b6f
Ben Minnikin <b.minnikin@gmail.com>
2020-12-21 08:09:03 +0000

    Add UsersAssignedToBugReport_Delete.sql stored procedure
    Deletes all instances of a user assigned to a specific bug report ID. Takes integer arguments for UserId and BugReportId.

---
+++ BugTracker/Database/Stored Procedures/UsersAssignedToBugReport_Delete.sql
@@ -0,0 +1,29 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[UsersAssignedToBugReport_Delete]    Script Date: 21/12/2020 07:09:36 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[UsersAssignedToBugReport_Delete]
+	@UserId int,
+	@BugReportId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DELETE FROM dbo.UsersAssignedToBugReport
+	WHERE UserId = @UserId AND BugReportId = @BugReportId
+END
+GO
