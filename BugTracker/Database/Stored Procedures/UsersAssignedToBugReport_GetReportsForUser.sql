5c594fef8a366daf8240156c7abda63a581b8c68
Ben Minnikin <b.minnikin@gmail.com>
2020-12-21 08:12:29 +0000

    Add UsersAssignedToBugReport_GetReportsForUser stored procedure
    Takes an integer parameter UserId and returns a collection of all bug reports that that user is assigned to.

---
+++ BugTracker/Database/Stored Procedures/UsersAssignedToBugReport_GetReportsForUser.sql
@@ -0,0 +1,30 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[UsersAssignedToBugReport_GetReportsForUser]    Script Date: 21/12/2020 08:09:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[UsersAssignedToBugReport_GetReportsForUser]
+	@UserId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT UsersAssignedToBugReport.BugReportId
+	FROM UsersAssignedToBugReport
+	WHERE UsersAssignedToBugReport.UserId = @UserId
+END
+GO
+
+
