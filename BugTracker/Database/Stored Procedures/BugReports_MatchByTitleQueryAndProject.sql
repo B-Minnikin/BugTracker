dc88ee7f332a5c1f39baf04873069a59396dc058
Ben Minnikin <b.minnikin@gmail.com>
2020-12-30 09:24:14 +0000

    Add BugReports_MatchByTitleQueryAndProject.sql stored procedure
    Takes a text query and project ID and returns the local bug report ID and title for matching bug report titles in the specified project.

---
+++ BugTracker/Database/Stored Procedures/BugReports_MatchByTitleQueryAndProject.sql
@@ -0,0 +1,31 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[BugReports_MatchByTitleQueryAndProject]    Script Date: 30/12/2020 09:22:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[BugReports_MatchByTitleQueryAndProject]
+	@Query nvarchar(max),
+	@ProjectId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT BugReport.LocalBugReportId, BugReport.Title
+	FROM BugReport
+	WHERE BugReport.Title LIKE ('%' + @Query + '%') AND
+		ProjectId = @ProjectId
+END
+GO
