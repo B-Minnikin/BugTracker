4114e475addfdfb1ee4d04ff734c78e4abb690ce
Ben Minnikin <b.minnikin@gmail.com>
2021-01-12 13:41:51 +0000

    Add BugReports_GetLinkedReports stored procedure
    Take a bug report ID and get all BugReport entries referenced.

---
+++ BugTracker/Database/Stored Procedures/BugReports_GetLinkedReports.sql
@@ -0,0 +1,26 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[BugReports_GetLinkedReports]    Script Date: 12/01/2021 13:39:40 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[BugReports_GetLinkedReports]
+	@BugReportId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT report.* FROM BugReport AS report
+	INNER JOIN BugReportLinks AS links
+	ON (report.BugReportId = links.BugReportId AND links.LinkToBugReportId = @BugReportId)
+		OR (report.BugReportId = links.LinkToBugReportId AND links.BugReportId = @BugReportId)
+END
+GO
+
+
