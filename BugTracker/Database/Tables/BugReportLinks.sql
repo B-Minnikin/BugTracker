eab775ed46c4ffa1f59d12f5c2611e5eefdeac64
Ben Minnikin <b.minnikin@gmail.com>
2021-01-12 13:33:05 +0000

    Add BugReportLinks SQL table
    Hold foreign key links between two bug reports IDs.

---
+++ BugTracker/Database/Tables/BugReportLinks.sql
@@ -0,0 +1,32 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  Table [dbo].[BugReportLinks]    Script Date: 12/01/2021 13:31:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[BugReportLinks](
+	[BugReportId] [int] NOT NULL,
+	[LinkToBugReportId] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[BugReportLinks]  WITH CHECK ADD  CONSTRAINT [FK_BugReportLinks_BugReport] FOREIGN KEY([BugReportId])
+REFERENCES [dbo].[BugReport] ([BugReportId])
+GO
+
+ALTER TABLE [dbo].[BugReportLinks] CHECK CONSTRAINT [FK_BugReportLinks_BugReport]
+GO
+
+ALTER TABLE [dbo].[BugReportLinks]  WITH CHECK ADD  CONSTRAINT [FK_BugReportLinks_BugReport1] FOREIGN KEY([LinkToBugReportId])
+REFERENCES [dbo].[BugReport] ([BugReportId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[BugReportLinks] CHECK CONSTRAINT [FK_BugReportLinks_BugReport1]
+GO
+
+
