2de70e96f041387f0aa0d5d119f2b1a89814351f
Ben Minnikin <b.minnikin@gmail.com>
2021-01-12 13:35:16 +0000

    Add BugReports_InsertLink stored procedure
    Take two bug report IDs and if a link between both does not exist, insert them into the BugReportLinks table.

---
+++ BugTracker/Database/Stored Procedures/BugReports_InsertLink.sql
@@ -0,0 +1,36 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[BugReports_InsertLink]    Script Date: 12/01/2021 13:33:33 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[BugReports_InsertLink]
+	@BugReportId int,
+	@LinkToBugReportId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF NOT EXISTS (SELECT * FROM dbo.BugReportLinks
+		WHERE (BugReportId = @BugReportId AND LinkToBugReportId = @LinkToBugReportId) OR
+		(BugReportId = @LinkToBugReportId AND LinkToBugReportId = @BugReportId))
+    -- Insert statements for procedure here
+	BEGIN
+		INSERT INTO dbo.BugReportLinks (BugReportId, LinkToBugReportId)
+		VALUES (@BugReportId, @LinkToBugReportId)
+	END
+END
+GO
+
+
