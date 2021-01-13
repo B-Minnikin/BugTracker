7b70645d4be807a6626a072964a058f6f4607e02
Ben Minnikin <b.minnikin@gmail.com>
2021-01-13 14:07:04 +0000

    Add Milestone class
    Hold milestone details in properties containing milestone's primary ID,  associated project ID, milestone Title and text Description, timestamp for milestone creation and an optional DueDate for completion.

---
+++ BugTracker/Models/Milestone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+	public class Milestone
+	{
+		public int MilestoneId { get; set; }
+		public int ProjectId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreationTime { get; set; } = DateTime.Now;
+		public DateTime DueDate { get; set; }
+	}
+}
