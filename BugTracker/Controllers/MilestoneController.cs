ce4f76e22a9dc5d42ed8ddda89f39e25157b1b52
Ben Minnikin <b.minnikin@gmail.com>
2021-01-14 15:38:33 +0000

    Add and implement Delete action method to MilestoneController
    Pass in milestoneId and send to the repository for deletion.

--- BugTracker/Controllers/MilestoneController.cs
+++ BugTracker/Controllers/MilestoneController.cs
@@ -1,4 +1,6 @@
 ﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,11 +14,18 @@ namespace BugTracker.Controllers
 	{
 		private readonly ILogger<MilestoneController> logger;
 		private readonly IProjectRepository projectRepository;
+		private readonly IAuthorizationService authorizationService;
+		private readonly IHttpContextAccessor httpContextAccessor;
 
-		public MilestoneController(ILogger<MilestoneController> logger, IProjectRepository projectRepository)
+		public MilestoneController(ILogger<MilestoneController> logger, 
+			IProjectRepository projectRepository,
+			IAuthorizationService authorizationService,
+			IHttpContextAccessor httpContextAccessor)
 		{
 			this.logger = logger;
 			this.projectRepository = projectRepository;
+			this.authorizationService = authorizationService;
+			this.httpContextAccessor = httpContextAccessor;
 		}
 
 		[HttpGet]
@@ -40,7 +49,22 @@ namespace BugTracker.Controllers
 				return RedirectToAction("Overview", "Projects", new { id = model.ProjectId });
 			}
 
+			logger.LogWarning($"Invalid Milestone model state");
 			return View(model);
+		}
+
+		public IActionResult Delete(int milestoneId)
+		{
+			var currentProjectId = HttpContext.Session.GetInt32("currentProject");
+			var authorizationResult = authorizationService.AuthorizeAsync(HttpContext.User, currentProjectId, "ProjectAdministratorPolicy");
+			if (authorizationResult.IsCompletedSuccessfully && authorizationResult.Result.Succeeded)
+			{
+				projectRepository.DeleteMilestone(milestoneId);
+
+				return RedirectToAction("Overview", "Projects", new { id = currentProjectId});
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
