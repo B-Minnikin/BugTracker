2c75b8d9ec40c0b11ffb0bb2174697418e00871c
Ben Minnikin <b.minnikin@gmail.com>
2021-01-23 02:32:56 +0000

    Add NewMilestoneViewModel class
    Contain form details for Milestone creation along with a list property containing chosen local IDs for chosen bug reports.

---
+++ BugTracker/ViewModels/NewMilestoneViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.ViewModels
+{
+	public class NewMilestoneViewModel
+	{
+		public int ProjectId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreationTime { get; set; } = DateTime.Now;
+		public DateTime DueDate { get; set; } = DateTime.Now.AddDays(7);
+
+		public List<int> MilestoneBugReportLocalIds { get; set; } = new List<int>();
+	}
+}
