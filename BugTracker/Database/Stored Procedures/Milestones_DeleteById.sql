ad33e7c5627f2f3bc2ca11ba8c4c487c8608a572
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 11:57:42 +0000

    Add Milestones_DeleteById.sql stored procedure
    Take a milestone ID and delete a matching entry from the Milestones table.

---
+++ BugTracker/Database/Stored Procedures/Milestones_DeleteById.sql
@@ -0,0 +1,23 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Milestones_DeleteById]    Script Date: 12/02/2021 11:56:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Milestones_DeleteById]
+	@MilestoneId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DELETE FROM dbo.Milestones
+	WHERE MilestoneId = @MilestoneId
+END
+GO
