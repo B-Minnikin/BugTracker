17974b91126b9eb55bb2f2d1b37e58deb505ed26
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 11:54:15 +0000

    Add Milestones_Insert.sql stored procedure
    Create a new milestone entry

---
+++ BugTracker/Database/Stored Procedures/Milestones_Insert.sql
@@ -0,0 +1,28 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Milestones_Insert]    Script Date: 12/02/2021 11:52:56 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Milestones_Insert]
+	@ProjectId int,
+	@Title nvarchar(max),
+	@Description nvarchar(max),
+	@CreationDate datetime2(7),
+	@DueDate datetime2(7)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	INSERT INTO dbo.Milestones(ProjectId, Title, Description, CreationDate, DueDate)
+	OUTPUT inserted.MilestoneId
+	VALUES (@ProjectId, @Title, @Description, @CreationDate, @DueDate)
+END
+GO
