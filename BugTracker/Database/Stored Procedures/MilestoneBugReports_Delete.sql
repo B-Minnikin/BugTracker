5151f7cf6701cd0159d35a29c13d9db92a978b1d
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 12:08:01 +0000

    Add MilestoneBugReports_Delete.sql stored procedure
    Delete all entries from MilestoneBugReports table where provided milestone ID and bug report ID are present.

---
+++ BugTracker/Database/Stored Procedures/MilestoneBugReports_Delete.sql
@@ -0,0 +1,24 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[MilestoneBugReports_Delete]    Script Date: 12/02/2021 12:06:46 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[MilestoneBugReports_Delete]
+	@MilestoneId int,
+	@BugReportId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DELETE FROM dbo.MilestoneBugReports
+	WHERE MilestoneId = @MilestoneId AND BugReportId = @BugReportId
+END
+GO
