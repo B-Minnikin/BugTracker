7101ff21779b0c3948eeb30d5a3794bcc41264c4
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 11:56:08 +0000

    Add Milestones_GetById.sql stored procedure
    Take a milestone ID and return a matching entry from Milestones table.

---
+++ BugTracker/Database/Stored Procedures/Milestones_GetById.sql
@@ -0,0 +1,22 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Milestones_GetById]    Script Date: 12/02/2021 11:54:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Milestones_GetById]
+	@MilestoneId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM dbo.Milestones WHERE MilestoneId = @MilestoneId
+END
+GO
