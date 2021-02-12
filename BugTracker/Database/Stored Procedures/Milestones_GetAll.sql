8b346217794f8a7dbc281f1eee2e238f0300a7ac
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 12:02:06 +0000

    Add Milestones_GetAll.sql stored procedure
    Take a project ID and return all milestone entries from Milestones table associated with that project.

---
+++ BugTracker/Database/Stored Procedures/Milestones_GetAll.sql
@@ -0,0 +1,22 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Milestones_GetAll]    Script Date: 12/02/2021 12:00:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Milestones_GetAll] 
+	@ProjectId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM dbo.Milestones WHERE ProjectId = @ProjectId
+END
+GO
