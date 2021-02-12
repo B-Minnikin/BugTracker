83763c66a01aac8bfaa131da61d1f6059b45d13b
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 11:39:32 +0000

    Add Milestones SQL table to Database/Tables
    Store a milestone - contains identity ID, associated project ID, the milestone Title, Description, and dates for creation and DueDate for holding a deadline.

---
+++ BugTracker/Database/Tables/Milestones.sql
@@ -0,0 +1,31 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  Table [dbo].[Milestones]    Script Date: 12/02/2021 11:36:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Milestones](
+	[MilestoneId] [int] IDENTITY(1,1) NOT NULL,
+	[ProjectId] [int] NOT NULL,
+	[Title] [nvarchar](max) NULL,
+	[Description] [nvarchar](max) NULL,
+	[CreationDate] [datetime2](7) NOT NULL,
+	[DueDate] [datetime2](7) NULL,
+ CONSTRAINT [PK_Milestones] PRIMARY KEY CLUSTERED 
+(
+	[MilestoneId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Milestones]  WITH CHECK ADD  CONSTRAINT [FK_Milestones_Projects] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Projects] ([ProjectId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Milestones] CHECK CONSTRAINT [FK_Milestones_Projects]
+GO
