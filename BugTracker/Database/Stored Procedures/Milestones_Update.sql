7f1e6a9f0515a02f58b62879dcbd085bb3e99b4a
Ben Minnikin <b.minnikin@gmail.com>
2021-02-12 12:00:16 +0000

    Add Milestones_Update.sql stored procedure
    Take milestone ID along with editable data and update all fields for the specific milestone ID with the new data in the Milestones table.

---
+++ BugTracker/Database/Stored Procedures/Milestones_Update.sql
@@ -0,0 +1,29 @@
+﻿USE [BugTrackerDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Milestones_Update]    Script Date: 12/02/2021 11:58:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Milestones_Update]
+	@MilestoneId int,
+	@Title nvarchar(MAX),
+	@Description nvarchar(MAX),
+	@CreationDate datetime2(7),
+	@DueDate datetime2(7)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE dbo.Milestones
+	SET Title = @Title, Description = @Description, CreationDate = @CreationDate, DueDate = @DueDate
+	OUTPUT inserted.MilestoneId
+	WHERE MilestoneId = @MilestoneId
+END
+GO
