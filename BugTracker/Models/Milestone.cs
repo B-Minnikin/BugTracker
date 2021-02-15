2a46887b4eb740fc76719972baa5f4d3674a1572
Ben Minnikin <b.minnikin@gmail.com>
2021-02-15 18:45:26 +0000

    Title of Milestone model is Required property

--- BugTracker/Models/Milestone.cs
+++ BugTracker/Models/Milestone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace BugTracker.Models
 	{
 		public int MilestoneId { get; set; }
 		public int ProjectId { get; set; }
+		[Required(ErrorMessage = "Please enter a title")]
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public DateTime CreationTime { get; set; } = DateTime.Now;
