81a4c4f8fd648a6a19e8ffb4c0b6d385877abc4c
Ben Minnikin <b.minnikin@gmail.com>
2021-04-13 18:58:38 +0100

    Update BugReports_MatchByTitleQueryAndProject.sql
    Return BugReportId in addition to local bug report ID.

--- BugTracker/Database/Stored Procedures/BugReports_MatchByTitleQueryAndProject.sql
+++ BugTracker/Database/Stored Procedures/BugReports_MatchByTitleQueryAndProject.sql
@@ -1,18 +1,11 @@
 ﻿USE [BugTrackerDB]
 GO
-
-/****** Object:  StoredProcedure [dbo].[BugReports_MatchByTitleQueryAndProject]    Script Date: 30/12/2020 09:22:27 ******/
+/****** Object:  StoredProcedure [dbo].[BugReports_MatchByTitleQueryAndProject]    Script Date: 13/04/2021 18:56:43 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
 CREATE PROCEDURE [dbo].[BugReports_MatchByTitleQueryAndProject]
 	@Query nvarchar(max),
 	@ProjectId int
@@ -23,9 +16,8 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	SELECT BugReport.LocalBugReportId, BugReport.Title
+	SELECT BugReport.BugReportId, BugReport.LocalBugReportId, BugReport.Title
 	FROM BugReport
 	WHERE BugReport.Title LIKE ('%' + @Query + '%') AND
 		ProjectId = @ProjectId
 END
-GO
