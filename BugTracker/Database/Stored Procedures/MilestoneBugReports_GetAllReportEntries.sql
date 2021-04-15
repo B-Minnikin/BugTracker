bdbecc447f7bca9e1e41c7399bc029650b821385
Ben Minnikin <b.minnikin@gmail.com>
2021-04-15 22:54:24 +0100

    Update MilestoneBugReports_GetAllReportEntries stored procedure
    Also return BugReportId.

--- BugTracker/Database/Stored Procedures/MilestoneBugReports_GetAllReportEntries.sql
+++ BugTracker/Database/Stored Procedures/MilestoneBugReports_GetAllReportEntries.sql
@@ -1,10 +1,8 @@
 ﻿USE [BugTrackerDB]
 GO
-
-/****** Object:  StoredProcedure [dbo].[MilestoneBugReports_GetAllReportEntries]    Script Date: 15/02/2021 16:18:20 ******/
+/****** Object:  StoredProcedure [dbo].[MilestoneBugReports_GetAllReportEntries]    Script Date: 15/04/2021 22:01:45 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -17,9 +15,8 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	SELECT dbo.BugReport.LocalBugReportId, dbo.BugReport.Title
+	SELECT dbo.BugReport.BugReportId, dbo.BugReport.LocalBugReportId, dbo.BugReport.Title
 	FROM dbo.BugReport
 	INNER JOIN dbo.MilestoneBugReports ON dbo.BugReport.BugReportId = dbo.MilestoneBugReports.BugReportId 
 	WHERE dbo.MilestoneBugReports.MilestoneId = @MilestoneId
 END
-GO
