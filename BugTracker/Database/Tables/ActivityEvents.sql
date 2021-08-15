e05cb12ee51926240fe9e045016d8a0a4b464d15
Ben Minnikin <b.minnikin@gmail.com>
2021-08-15 15:16:37 +0100

    Add ActivityEvents sql file
    Store all objects with a base class of Activity inside the ActivityEvents table leaving empty, optional cells as NULL.

---
+++ BugTracker/Database/Tables/ActivityEvents.sql
@@ -0,0 +1,30 @@
+USE [BugTrackerDB]
+GO
+
+/****** Object:  Table [dbo].[ActivityEvents]    Script Date: 15/08/2021 15:14:29 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ActivityEvents](
+	[ActivityId] [int] IDENTITY(1,1) NOT NULL,
+	[Timestamp] [datetime2](7) NOT NULL,
+	[ProjectId] [int] NOT NULL,
+	[MessageId] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[BugReportId] [int] NULL,
+	[AssigneeId] [int] NULL,
+	[LinkedBugReportId] [int] NULL,
+	[NewBugReportStateId] [int] NULL,
+	[PreviousBugReportStateId] [int] NULL,
+	[BugReportCommentId] [int] NULL,
+	[MilestoneId] [int] NULL,
+ CONSTRAINT [PK_ActivityEvents] PRIMARY KEY CLUSTERED 
+(
+	[ActivityId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
